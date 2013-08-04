53175530e62e7a8d4782a03b660f4af6005a8cb6
Manik Tharaka <mgalkissa@gmail.com>
2013-08-05 00:24:19 +0530

    added a method to write data still skeleton

--- mine2.py
+++ mine2.py
@@ -31,17 +31,33 @@ def extract(var,entry,di):
 
 
 
+
+def minePage(dom):
+    varia=["Code:","Added:","Make:","Model:","No:","Year:","Location:","Options:","Price:","Info:"]
+    di={}
+    ads={}
+
+    for entry in dom.cssselect('.BuyDataTD'):
+        [extract(var,entry,di) for var in varia]
+        if len(di)==10 and di['Code:'] not in ads.keys():
+            ads[di['Code:']]=di
+            #print di['Code:']
+            #            print ads[di['Code:']]
+
+    return ads
+
+
+def writeToFile(data):
+    open("data.csv","w")
+
+    for k in data.keys():
+         print k
+         print data[k]
+         print "\n"
+
+
 #html = requests.get("http://www.autolanka.com/Buy.asp").content
 html=open('index','r')
 dom = lxml.html.fromstring(html.read())
-
-
-varia=["Code:","Added:","Make:","Model:","No:","Year:","Location:","Options:","Price:","Info:"]
-di={}
-ads={}
-
-for entry in dom.cssselect('.BuyDataTD'):
-    [extract(var,entry,di) for var in varia]
-    if len(di)==10 and di['Code:'] not in ads.keys():
-        ads[di['Code:']]=di
-        print di['Code:']+" ok"
+data=minePage(dom)
+writeToFile(data)
