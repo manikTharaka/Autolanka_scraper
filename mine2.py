a6e62fbeb664baae51e060eb2319eebc40d54f64
Manik Tharaka <mgalkissa@gmail.com>
2013-08-04 22:39:16 +0530

    non recursive mining for a single page

--- mine2.py
+++ mine2.py
@@ -6,10 +6,10 @@ import lxml.html
 import time
 
 
+
 def extract(var,entry,di):
     if var in entry.text_content():
         word=entry.text_content().split()
-        print word
         index=word.index(var)+1
         if len(word) > index:
             while len(word) > index:
@@ -18,12 +18,18 @@ def extract(var,entry,di):
         else:
             val="NULL"
 
-        #print var+val+"\n"
         if val == None:
             return
-        
+
+        if var=="Added:":
+            var="Date Added:"
+        if var=="No:":
+            var="Model No:"
+
         di[var]=val
         return val
+
+
 
 #html = requests.get("http://www.autolanka.com/Buy.asp").content
 html=open('index','r')
@@ -33,14 +39,9 @@ dom = lxml.html.fromstring(html.read())
 varia=["Code:","Added:","Make:","Model:","No:","Year:","Location:","Options:","Price:","Info:"]
 di={}
 ads={}
+
 for entry in dom.cssselect('.BuyDataTD'):
     [extract(var,entry,di) for var in varia]
-    if len(di)==10:
-        print di
-
-
-
-    #if (len(di)==10) and not(di['Code:'].replace("Code:","") in ads):
-    #   ads[di['Code:'].replace("Code:","")]=di
-    #print ads
-    #print"\n"
+    if len(di)==10 and di['Code:'] not in ads.keys():
+        ads[di['Code:']]=di
+        print di['Code:']+" ok"
